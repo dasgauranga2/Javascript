375729e085addb6fe8f1dc407cb952ad1b192eed
Gauranga Das <dasgauranga2@gmail.com>
2021-01-12 16:43:21 +0530

    Fundamentals Part 2 updated

---
+++ Fundamentals Part 2/arrow_function.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// arrow function
+// in the parentheses there are function parameters
+// after the '=>' sign is the function body
+const calc_age = (birth_year) => { 
+    return 2020-birth_year;
+};
+console.log(calc_age(1998));
